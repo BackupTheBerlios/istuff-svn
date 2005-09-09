68a8a72f752c89ed7d2a507b2318f4abe698c915
eugen_yu <eugen_yu@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2005-09-09 21:22:48 +0000

    Another Knobserver. This one is based on Cocoa.

---
+++ trunk/Hardware Proxies/KnobServer2/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KnobServer
+//
+//  Created by Eugen Yu on 9/9/05.
+//  Copyright __MyCompanyName__ 2005. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
