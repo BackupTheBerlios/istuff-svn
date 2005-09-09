1cc97f9dbf4bae2a4f1782cdf5697118b2c82372
eugen_yu <eugen_yu@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2005-09-09 19:02:47 +0000

    Added scripts for a knob server that contains a integer field called delta

---
+++ trunk/Patch Panel Manager/Demo/Knob/run.sh
@@ -0,0 +1,33 @@
+#exit 0 if not enough arguments
+if [$# -lt 2]
+then
+	echo
+	echo Usage: run.sh EVENTHEAP_NAME MACHINE_NAME
+	echo MACHINE_NAME is where the AppleScriptProxy shall be running
+	echo Beware that MACHINE_NAME should correspond to the global variable "currentHostName" of the KnobScript.pps- patch panel script
+	echo
+	exit 1
+fi
+EHEAPNAME=$1
+MACHINENAME=$2
+
+LIBPATH=../../../Lib
+#if [ -e GlobalVariables.jph ]
+#then  rm *.jph
+#fi
+rm *.jph
+
+echo
+echo Start Apple Script Server
+osascript ../dummy.pps
+./startEHtoAS.sh $EHEAPNAME $MACHINENAME
+echo
+echo Start pacth panel
+java -server -Djava.library.path=./lib -classpath $LIBPATH/iROS.jar:$LIBPATH/jph.jar iwork.patchpanel.Intermediary $EHEAPNAME &
+echo
+echo Start Knob Proxy
+#....to be cont
+echo
+echo Load patch script
+java -cp $LIBPATH/iROS.jar:$LIBPATH/jph.jar iwork.patchpanel.manager.PPManager -s KnobScript.pps $EHEAPNAME &
+
