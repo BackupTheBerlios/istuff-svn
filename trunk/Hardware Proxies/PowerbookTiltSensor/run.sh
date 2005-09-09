212b48905d31582aee10c3cc218c240610666bb6
eugen_yu <eugen_yu@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2005-09-09 09:49:47 +0000

    Modified the run script so that eheap name can be specified

--- trunk/Hardware Proxies/PowerbookTiltSensor/run.sh
+++ trunk/Hardware Proxies/PowerbookTiltSensor/run.sh
@@ -1,2 +1,7 @@
-./amstracker -u 0.2 | java -cp "../../Lib/iROS.jar":. amsProxy localhost
+EHEAPSERVER=localhost
+if [ $# -ge 1 ]
+then 
+    EHEAPSERVER=$1
+fi
+./amstracker -u 0.2 | java -cp "../../Lib/iROS.jar":. amsProxy $EHEAPSERVER
 # -u = update rate, update every 0.2 seconds
