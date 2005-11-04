309ab68bd4c6802e0588232263c124b9bff65501
eugen_yu <eugen_yu@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2005-11-04 10:53:49 +0000

    This version also put the returning value back on the event heap

--- trunk/Software Proxies/EHtoAppleScript/EHtoAppleScript.java
+++ trunk/Software Proxies/EHtoAppleScript/EHtoAppleScript.java
@@ -1,11 +1,17 @@
 import iwork.eheap2.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
+
 
 public class EHtoAppleScript
 {
 	private EventHeap eheap;
 	public String hostname;
+	/* EY: To operate on multiple machine on the same account, a mechanism for mutual exclusion is needed.
+	   With the following line, the programm manipulates the local files on multiple machines.
+	public static String SCRIPT_FILE_NAME = "/tmp/script.txt";
+	*/
 	public static String SCRIPT_FILE_NAME = "script.txt";
 	private Event latest = null;
 	
@@ -52,30 +58,71 @@ public class EHtoAppleScript
 		}
 	}
 
+	/**
+	To execute a script. It writes the content of "script" into a file, then uses osascript command to invoke the execution.
+	*/
 	public void execute(String script){
 		File scriptFile = new File(SCRIPT_FILE_NAME);
 		
 
 		try{
-			BufferedWriter bufWrtr = new BufferedWriter(new FileWriter(scriptFile)); 
-
+			BufferedWriter bufWrtr = new BufferedWriter(new FileWriter(scriptFile));
 			String line = null; 
 			bufWrtr.write(script); 
 			bufWrtr.close(); 
 
+			ArrayList cmd = new ArrayList();
+			cmd.add("/usr/bin/osascript");
+			cmd.add(scriptFile.getPath());
+			//Creating a process
+			String[] cmdArray = (String[]) cmd.toArray(new String[0]);
+			Process proc = Runtime.getRuntime().exec(cmdArray);
+			
+			//Execute the process
+			proc.waitFor();
+			
+			//Creating the output after execution
+			Event outEvent = new Event("AppleScript");
+			StringBuffer output = new StringBuffer();
+			
+			if (proc.exitValue() != 0){
+				outEvent.addField("type","error");
+				BufferedReader err = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+				while ((line = err.readLine())!= null){
+					output.append(line + "\n");
+				}
+			}
+			else{
+				outEvent.addField("type","result");
+				BufferedReader out = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+				while ((line = out.readLine())!= null){
+					output.append(line + "\n");
+				}
+			}
+			
+			
+			//System.out.println(this.getClass()+":"  + output.toString());
+			outEvent.addField("comment","return value of script execution");
+			outEvent.addField("content",output.toString());
+			eheap.putEvent(outEvent);
+			
+			/* +++++++++++++++++++++++++		
+			//EY: This is the old version that does not put return value back on the heap
 			String exec = "osascript " + SCRIPT_FILE_NAME;
 			System.out.println(exec);
-            Process proc = Runtime.getRuntime().exec(exec);
+          		Process proc = Runtime.getRuntime().exec(exec);
 
-            // get its output (your input) stream
+            		// get its output (your input) stream
 
-            DataInputStream in = new DataInputStream(
+            		DataInputStream in = new DataInputStream(
                                           proc.getInputStream());
 			String str;
-            while ((str = in.readLine()) != null) {
-                System.out.println(str);
+            		while ((str = in.readLine()) != null) {
+                		System.out.println(str);
 			}
 			proc.waitFor();
+			+++++++++++++++++++++++++*/
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
@@ -115,5 +162,15 @@ public class EHtoAppleScript
 			System.out.println("Usage: EHtoAppleScript <event heap name> [machine name]");
 			System.exit(-1);
 		}
+     }
+     
+     
+     
+     
+     class AppleScriptException extends Exception{
+     	
+     	public AppleScriptException(String msg){
+     		super(msg);
+     	}
      }
 } 
