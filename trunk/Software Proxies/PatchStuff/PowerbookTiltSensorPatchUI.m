2d868fa10a86a2e395209f3c95e8c68af28f1dda
reiny <reiny@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2006-01-17 16:50:23 +0000

    Replacement of the PRoxyGUI

--- trunk/Software Proxies/PatchStuff/PowerbookTiltSensorPatchUI.m
+++ trunk/Software Proxies/PatchStuff/PowerbookTiltSensorPatchUI.m
@@ -10,6 +10,8 @@
 
 
 @implementation PowerbookTiltSensorPatchUI
+// this method is called when a patch is inserted and 
+// the inspector is  shown for the first time
 + (id)viewNibName
 {
 	NSLog(@"in PowerbookTiltSensorPatchUI: viewNibName");
@@ -17,18 +19,24 @@
 }
 
 - (void)_updateSources{
+	NSLog(@"in PowerbookTiltSensorPatchUI: updateSources");
+
 
 }
 
 - (void)deviceMenu:(id)fp8{
+	NSLog(@"in PowerbookTiltSensorPatchUI: deviceMenu");
 
 }
 
 - (void)sourceMenu:(id)fp8
 {
+	NSLog(@"in PowerbookTiltSensorPatchUI: sourceMenu");
 
 }
 
+// This method is always called  when the inspector is called
+// and maybe when tab is called for the first time...
 - (void)setupViewForPatch:(id)fp8
 {
 	NSLog(@"in PowerbookTiltSensorPatchUI: setupViewForPatch");
