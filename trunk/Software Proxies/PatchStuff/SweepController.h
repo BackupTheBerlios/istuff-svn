b4ea5970f7b109250224ba066577d4368d357396
reiny <reiny@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2006-04-19 21:26:58 +0000

    Renamed for consistency with Proxy Manager

---
+++ trunk/Software Proxies/PatchStuff/SweepController.h
@@ -0,0 +1,25 @@
+//
+//  SweepPatch.h
+//  QCiStuff
+//
+//  Created by Rafael Ballagas on 11/7/05.
+//  Refactored by René Reiners on 03/02.06
+//  Copyright 2005 Media Computing Group, RWTH Aachen University, Germany. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "IStuffProviderPatch.h"
+
+@interface SweepPatch : iStuffProviderPatch {
+	
+	QCNumberPort *outputdX;
+    QCNumberPort *outputdY;
+    QCNumberPort *outputdrZ;
+	QCBooleanPort *outputClick;
+
+}
+
+- (id) initWithIdentifier:(id)fp8;
+- (BOOL) execute:(id)fp8 time:(double)fp12 arguments:(id)fp20;
+
+@end
