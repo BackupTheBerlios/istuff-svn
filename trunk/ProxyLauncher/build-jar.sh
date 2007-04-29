acf055ad7a107ae7ac6ff99b59fb0256b9814674
hemig <hemig@2a53cb5c-8ff1-0310-8b75-b3ec22923d26>
2007-04-29 07:54:59 +0000

    adepted buildscript, using 'svn export' instead of copying whole folders - Tim

--- trunk/ProxyLauncher/build-jar.sh
+++ trunk/ProxyLauncher/build-jar.sh
@@ -1,16 +1,39 @@
+# create directoryname
 REL_DIR=$(date +%Y.%m.%d)
+
+# clear target and create correct folder
 rm -rf ../../release/$REL_DIR
 mkdir ../../release/$REL_DIR
 mkdir ../../release/$REL_DIR/iStuff/
+
+# build the java project
 javac -classpath ./:AppleJavaExtensions.jar *.java
+
+# move/copy all important files for the jar-target into a clean subfolder "build"
 rm -rf build
 mkdir build
-cp -r addons/* build/
+cp -r addons/*.class build/
+svn export addons/iwork build/iwork
 mv *.class build/
-cp -r manifest.txt *.prefs *.xml JUnit com org icons javax build/
+cp -r manifest.txt *.prefs *.xml build/
+svn export JUnit build/JUnit
+svn export com build/com
+svn export org build/org
+svn export icons build/icons
+svn export javax build/javax
+
+# goto the build dir and create the jar - put it into tha target dir
 cd build
 jar cvfm ../../../release/$REL_DIR/iStuff/iStuff.jar manifest.txt  *
 cd ..
+
+# clean up
 rm -rf build
+
+# copy some surroundings into the target dir
 cp start* ../../release/$REL_DIR/iStuff/
-cp -r Hardware\ Proxies Software\ Proxies ../../release/$REL_DIR/iStuff/
+svn export Hardware\ Proxies ../../release/$REL_DIR/iStuff/Hardware\ Proxies
+svn export Software\ Proxies ../../release/$REL_DIR/iStuff/Software\ Proxies
+
+# done
+echo "Done\n"
